7f2c70f721f92df33cb11b396ec4c3d3bc56a652
Dustan Kasten <dustan.kasten@gmail.com>
2015-10-26 15:37:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,217 @@
+# babel-plugin-react-cssmoduleify
+
+For those who have tasted [CSS Modules](https://github.com/css-modules/css-modules)
+it is hard to imagine life without it. At Walmart we have a large number of core
+components that still use traditional CSS classNames and global CSS. To allow
+for teams to take advantage of CSS Modules, this babel plugin is used to convert
+all global styles into CSS Modules.
+
+## Usage
+
+* `npm install --save babel-plugin-react-cssmoduleify`
+
+.babelrc
+
+```js
+{
+  "plugins": [
+    "react-cssmoduleify",
+  ],
+  "extra": {
+    "react-cssmoduleify": {
+      "path": "node_modules/@walmart/wmreact-",
+      "cssmodule": "client/styles/base.styl"
+    }
+  },
+}
+```
+
+#### Options:
+
+* `path`: Regex to test if a file should be transformed.
+* `cssmodule`: path from `process.cwd()` to global CSS file
+
+## Example
+
+### Before
+
+```js
+"use strict";
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var MyComponent = (function (_React$Component) {
+  _inherits(MyComponent, _React$Component);
+
+  function MyComponent(props) {
+    _classCallCheck(this, MyComponent);
+
+    _get(Object.getPrototypeOf(MyComponent.prototype), "constructor", this).call(this, props);
+  }
+
+  _createClass(MyComponent, [{
+    key: "_renderFlyoutConent",
+    value: function _renderFlyoutConent() {
+      if (this.state.loading) {
+        return _react2["default"].createElement(
+          "div",
+          { className: "lists-spinner" },
+          _react2["default"].createElement(_walmartWmreactContainers.Spinner, { loading: true })
+        );
+      }
+      return _react2["default"].createElement(_addToListFlyout2["default"], { lists: this.state.lists, isSignedIn: this.props.isSignedIn });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var addToListTrigger = _react2["default"].createElement(
+        _walmartWmreactInteractive.Button,
+        {
+          className: (0, _classnames2["default"])("btn-inverse")
+        },
+        this.props.label
+      );
+      if (this.props.addToListTrigger) {
+        addToListTrigger = cloneElement(this.props.addToListTrigger, {});
+      }
+      return _react2["default"].createElement(
+        "div",
+        { className: (0, _classnames2["default"])(this.props.className) },
+        _react2["default"].createElement(
+          _walmartWmreactContainers.Flyout,
+          {
+            trigger: addToListTrigger,
+            direction: this.props.direction,
+            onActiveChange: this._onAddToListClicked.bind(this)
+          },
+          this._renderFlyoutConent()
+        )
+      );
+    }
+  }]);
+
+  return MyComponent;
+})(_react2["default"].Component);
+
+MyComponent.propTypes = {
+  className: _react2["default"].PropTypes.string,
+  label: _react2["default"].PropTypes.string,
+  direction: _react2["default"].PropTypes.string,
+  isSignedIn: _react2["default"].PropTypes.bool,
+  addToListTrigger: _react2["default"].PropTypes.func
+};
+
+exports["default"] = MyComponent;
+module.exports = exports["default"];
+```
+
+### After
+
+```js
+"use strict";
+
+var _react = require("react");
+var _cssmodules = require("/Users/dkasten/project/client/styles/base.styl");
+
+var _react2 = _interopRequireDefault(_react);
+
+var MyComponent = (function (_React$Component) {
+  _inherits(MyComponent, _React$Component);
+
+  function MyComponent(props) {
+    _classCallCheck(this, MyComponent);
+
+    _get(Object.getPrototypeOf(MyComponent.prototype), "constructor", this).call(this, props);
+  }
+
+  _createClass(MyComponent, [{
+    key: "_renderFlyoutConent",
+    value: function _renderFlyoutConent() {
+      if (this.state.loading) {
+        return _react2["default"].createElement(
+          "div",
+          { className: _cssmodules["lists-spinner"] },
+          _react2["default"].createElement(_walmartWmreactContainers.Spinner, { loading: true })
+        );
+      }
+      return _react2["default"].createElement(_addToListFlyout2["default"], { lists: this.state.lists, isSignedIn: this.props.isSignedIn });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var addToListTrigger = _react2["default"].createElement(
+        _walmartWmreactInteractive.Button,
+        {
+          className: (0, _classnames2["default"])(_cssmodules["btn-inverse"])
+        },
+        this.props.label
+      );
+      if (this.props.addToListTrigger) {
+        addToListTrigger = cloneElement(this.props.addToListTrigger, {});
+      }
+      return _react2["default"].createElement(
+        "div",
+        { className: (0, _classnames2["default"])(this.props.className) },
+        _react2["default"].createElement(
+          _walmartWmreactContainers.Flyout,
+          {
+            trigger: addToListTrigger,
+            direction: this.props.direction,
+            onActiveChange: this._onAddToListClicked.bind(this)
+          },
+          this._renderFlyoutConent()
+        )
+      );
+    }
+  }]);
+
+  return MyComponent;
+})(_react2["default"].Component);
+
+MyComponent.propTypes = {
+  className: _react2["default"].PropTypes.string,
+  label: _react2["default"].PropTypes.string,
+  direction: _react2["default"].PropTypes.string,
+  isSignedIn: _react2["default"].PropTypes.bool,
+  addToListTrigger: _react2["default"].PropTypes.func
+};
+
+exports["default"] = MyComponent;
+module.exports = exports["default"];
+```
+
+
+## Caveats
+
+This assumes that you can get all exports into one file. Most CSS Preprocessors
+build on the global nature of CSS and have their own internal global worlds.
+Importing all of your traditional files into a single file in Stylus or Sass
+will likely accomplish this.
+
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Walmart Labs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
---
+++ index.js
@@ -0,0 +1,373 @@
+/**
+ * Babel plugin to transform React className property values to CSS Module style
+ * lookups
+ */
+export default function ({Plugin, parse, types: t}) {
+  const optionsKey = 'react-cssmoduleify';
+  let cssModuleId;
+
+  function warn(...args) {
+    console.warn(...args);
+    console.warn(
+      'Please report this at https://github.com/walmartreact/' +
+      'babel-plugin-react-cssmoduleify/issues'
+    );
+  }
+
+  function isValidOptions(options) {
+    return typeof options === 'object' && options.path && options.cssmodule;
+  }
+
+  /**
+   * Enforces plugin options to be defined and returns them.
+   */
+  function getPluginOptions(file) {
+    if (!file.opts || !file.opts.extra) {
+      return;
+    }
+
+    var pluginOptions = file.opts.extra[optionsKey];
+
+    if (!isValidOptions(pluginOptions)) {
+      throw new Error(
+        'babel-plugin-react-css-modules requires that you specify extras["' +
+        optionsKey + '"] in .babelrc ' + 'or in your Babel Node API call ' +
+        'options with "path" and "cssmodule" keys.'
+      );
+    }
+    return pluginOptions;
+  }
+
+  /**
+  * Transform an AST Node representing a literal string property to an
+  * appropriate CSS module-style memberExpression
+  *
+  * { className: 'hello'} => className: _styles['hello'] }
+  * { className: 'hello world'} => className: _styles['hello'] + ' ' + _styles['world'] }
+  */
+  const mutateStringPropertyToCSSModule = (prop) => {
+    const parts = prop.value.value.split(' ').map(v => {
+      return t.memberExpression(
+        cssModuleId,
+        t.literal(v),
+        true
+      )
+    });
+
+    if (parts.length === 1) {
+      prop.value = parts[0];
+    } else {
+      prop.value = parts.reduce((ast, cur, i) => {
+        if (!ast) return cur;
+
+        return t.binaryExpression(
+          '+',
+          t.binaryExpression('+', ast, t.literal(' ')),
+          cur
+        )
+      })
+    }
+  };
+
+  const isClassnamesIsh = (node) => {
+    const reClassnames = /classnames/i;
+
+    if (node.type !== 'CallExpression') {
+      return false;
+    }
+
+    if (node.callee.type === 'Identifier') {
+      return reClassnames.test(node.callee.name);
+    }
+
+    let thing = node.callee;
+
+    // babel does this stuff
+    if (node.callee.type === 'SequenceExpression') {
+      if (node.callee.expressions.length !== 2) return false;
+      if (node.callee.expressions[0].value !== 0) return false;
+
+      thing = node.callee.expressions[1];
+    }
+
+    if (thing.type === 'Identifier') {
+      return reClassnames.test(thing.name);
+    } else if (thing.type === 'MemberExpression') {
+      return reClassnames.test(thing.object.name);
+    }
+  };
+
+  const convertStringKeysToComputedProperties = (node) => {
+    // return node;
+
+    node.properties.forEach(p => {
+      p.key = t.memberExpression(
+        cssModuleId,
+        t.literal(p.key.value),
+        true
+      );
+      p.computed = true;
+    });
+    return node;
+  };
+
+  const mutateClassnamesCall = (node, scope) => {
+    node.arguments = node.arguments.map(v => {
+      if (v.type === 'Identifier') {
+        const bindings = scope.bindings[v.name];
+        if (!bindings) {
+          return t.memberExpression(
+            cssModuleId,
+            t.literal(v.name),
+            true
+          );
+        }
+        v = bindings.path.node;
+        if (v.type === 'VariableDeclarator') {
+          v = v.init;
+        }
+      }
+
+      if (v.type === 'CallExpression') return v;
+
+      if (
+        v.type === 'Literal' ||
+        v.type === 'BinaryExpression' ||
+        v.type === 'ConditionalExpression'
+      ) {
+        return t.memberExpression(cssModuleId, v, true);
+      }
+
+      if (v.type === 'ObjectExpression') {
+        return convertStringKeysToComputedProperties(v);
+      }
+
+      if (v.type === 'MemberExpression') {
+        return v;
+      }
+
+      warn('WARNING(TODO): handle mutating classnames call for %s.', v.type);
+      return v;
+    });
+  };
+
+  const computedOrIdentifier = (node) =>
+    t.logicalExpression(
+      '||',
+      t.memberExpression(cssModuleId, node, true),
+      node
+    );
+
+  const _handleBinaryExpression = (node) => {
+    if (node.value === '') return node;
+
+    if (
+      node.type === 'MemberExpression'
+    ) {
+      if (
+        node.object.name === 'props' || (
+          node.object.property &&
+          node.object.property.name === 'props'
+        )
+      ) {
+        return node;
+      }
+
+      return t.memberExpression(
+        cssModuleId,
+        node,
+        true
+      );
+    }
+
+    if (node.type === 'ConditionalExpression') {
+      node.consequent = _handleBinaryExpression(node.consequent);
+      node.alternate = _handleBinaryExpression(node.alternate);
+      return node;
+    }
+
+    const parts = node.value.split(' ');
+    if (parts.length === 1) {
+      return t.memberExpression(
+        cssModuleId,
+        t.literal(parts[0]),
+        true
+      );
+    }
+
+    return parts.reduce((ast, n) => {
+      if (n === '') {
+        return ast;
+      }
+
+      if (!ast) {
+        return typeof n === 'object' ? n
+          : t.binaryExpression('+',
+              t.memberExpression(
+                cssModuleId,
+                t.literal(n),
+                true
+              ),
+              t.literal(' ')
+            );
+      }
+
+      return t.binaryExpression(
+        '+',
+        t.binaryExpression('+', ast, t.literal(' ')),
+        t.binaryExpression('+', t.literal(n), t.literal(' '))
+      )
+    }, null);
+  }
+
+  const handleBinaryExpressionProp = (prop, node, scope, file) => {
+    if (prop.value.left.type === 'Literal' || prop.value.right.type === 'Literal') {
+      const left = _handleBinaryExpression(prop.value.left);
+      const right = _handleBinaryExpression(prop.value.right);
+      prop.value.left = left;
+      prop.value.right = right;
+    }
+    else {
+      warn('Unhandled BinaryExpression property at %s#%s.', file.opts.filename, prop.loc.start.row);
+    }
+
+    return prop;
+
+    if (prop.value.right.type !== 'Literal') {
+      prop.value.right = t.memberExpression(
+        cssModuleId,
+        prop.value.right,
+        true
+      );
+    }
+    else {
+      prop.value.right = handleProp({key: {name: 'className'}, value: prop.value.right}, node, scope, file).value;
+    }
+  }
+
+  const handleProp = (prop, node, scope, file) => {
+    if (prop.key.name !== 'className') return prop;
+
+    if (prop.value.type === 'Identifier') {
+      const binding = scope.bindings[prop.value.name];
+
+      if (!binding) {
+        prop.value = computedOrIdentifier(prop.value);
+        return prop;
+      }
+
+      const {path} = binding;
+      if (path.node.init.value) {
+        mutateStringPropertyToCSSModule(prop);
+      }
+      else if (isClassnamesIsh(path.node.init)) {
+        mutateClassnamesCall(path.node.init, scope);
+      }
+
+      return prop;
+    }
+
+
+    if (prop.value.value) {
+      mutateStringPropertyToCSSModule(prop);
+    }
+    else if (isClassnamesIsh(prop.value)) {
+      mutateClassnamesCall(prop.value, scope);
+    }
+    else if (prop.value.type === 'BinaryExpression') {
+      return handleBinaryExpressionProp(prop, node, scope, file);
+      return prop;
+    }
+    else if (prop.value.type === 'ConditionalExpression') {
+      prop.value.consequent = handleProp(
+        {key: {name: 'className'},
+         value: prop.value.consequent
+        }, node, scope, file).value;
+
+      prop.value.alternate = handleProp(
+        {key: {name: 'className'},
+         value: prop.value.alternate
+        }, node, scope, file).value;
+
+      return prop;
+    }
+    else {
+      if (
+        prop.value.type === 'MemberExpression' ||
+        prop.value.type === 'CallExpression' ||
+        prop.value.type === 'Literal' && prop.value.value === ''
+      ) {
+        return prop;
+      }
+
+      warn(
+        '\n\n' +
+        '==================================================\n' +
+        'WARNING: unhandled className in %s#%s.\n',
+        file.opts.filename,
+        node.loc.start.line
+      );
+    }
+
+    return prop;
+  };
+
+  return new Plugin('css-moduleify-plugin', {
+    visitor: {
+      CallExpression(node, parent, scope, file) {
+        if (!this.state.shouldTransform) return;
+
+        if (
+          !node.callee.property || (
+            node.callee.property.name !== 'createElement' ||
+            node.callee.object.name === 'document'
+          )
+        ) return;
+
+        const {properties} = node.arguments[1];
+        if (!properties) return;
+        node.arguments[1].properties = properties.map(prop => handleProp(prop, node, scope, file));
+      },
+
+      Program: {
+        enter (node, parent, scope, file) {
+          const opts = getPluginOptions(file);
+
+          if (new RegExp(opts.path).test(file.opts.filename)) {
+            // console.log('Program file', file.opts.filename);
+            this.state.shouldTransform = true;
+            cssModuleId = scope.generateUidIdentifier('cssmodule');
+          }
+        },
+
+        exit(node, parent, scope, file) {
+          if (this.state.shouldTransform !== true) return;
+
+          const {cssmodule} = getPluginOptions(file);
+
+          const cssRequire = t.variableDeclaration('var', [
+            t.variableDeclarator(
+              cssModuleId,
+              t.callExpression(
+                t.identifier('require'),
+                [t.literal(process.cwd() + '/' + cssmodule)]
+              )
+            )
+          ]);
+          let insertIndex;
+          node.body.find((n, i) => {
+            const result = n.type === 'VariableDeclaration'
+              && n.declarations[0].init.type === 'CallExpression'
+              && n.declarations[0].init.callee.name === 'require';
+
+            insertIndex = i;
+            return result;
+          });
+
+          node.body.splice(insertIndex, 0, cssRequire);
+        }
+      }
+    }
+  });
+}
+
